8b295124e91d0abf6000a3204c9202ecb1d44bd2
angelina dorasheva <aydorash@mts.ru>
2021-12-15 23:48:22 +0300

    Выпиливаем http, запускаем сервер при помощи Express

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,45 +1,27 @@
 'use strict';
 
+const express = require(`express`);
+const {Router} = require(`express`);
+const router = new Router();
+
 const chalk = require(`chalk`);
-const http = require(`http`);
 const fs = require(`fs`).promises;
-const {Port, MOCKS_FILE_NAME, HttpCode} = require(`../../constants`);
+const {Port, MOCKS_FILE_NAME} = require(`../../constants`);
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
+const app = express();
 
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
+app.use(express.json());
+app.use(router);
 
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(MOCKS_FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
+const getPosts = async () => {
+  try {
+    const fileContent = await fs.readFile(MOCKS_FILE_NAME);
+    const mocks = JSON.parse(fileContent);
+    const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+    return `<ul>${message}</ul>`;
+  } catch (e) {
+    console.error(chalk.red(e));
+    return [];
   }
 };
 
@@ -49,14 +31,11 @@ module.exports = {
     const [customPort] = args || Port.DEFAULT_SERVICE_PORT;
     const port = Number.parseInt(customPort, 10) || Port.DEFAULT_SERVICE_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          console.error(chalk.red(`Ошибка при создании сервера`, err));
-        } else {
-          console.info(chalk.green(`Ожидаю соединений на ${port}`));
-        }
-      });
+    router.get(`/posts`, (async (req, res) => {
+      const posts = await getPosts();
+      res.send(posts);
+    }));
+
+    app.listen(port, () => console.info(chalk.blue(`Сервер запущен на порту: ${port}`)));
   }
 };
